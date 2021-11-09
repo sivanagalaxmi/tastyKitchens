ce60e10a21bcdacde3ad98c58c0fe0cae7cbffdb
sivanagalaxmi <91450385+sivanagalaxmi@users.noreply.github.com>
2021-11-09 19:54:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import {Switch, Route, Redirect} from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
+import Login from './components/Login'
+import Home from './components/Home'
+import Cart from './components/Cart'
+import RestaurantDetails from './components/RestaurantDetails'
+import NotFound from './components/NotFound'
+import './App.css'
+
+/* const sortByOptions = [
+  {
+    id: 0,
+    displayText: 'Highest',
+    value: 'Highest',
+  },
+  {
+    id: 2,
+    displayText: 'Lowest',
+    value: 'Lowest',
+  },
+] */
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/cart" component={Cart} />
+    <ProtectedRoute
+      exact
+      path="/restaurant/:id"
+      component={RestaurantDetails}
+    />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
+)
+
+export default App
